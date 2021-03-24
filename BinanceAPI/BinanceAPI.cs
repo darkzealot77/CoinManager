1ea7970bef463fee3529cc2c1af343ddbf82974e
Alucard TheDark <darkzealot77@hotmail.com>
2021-03-24 14:55:11 +0100

    Création appli

---
+++ BinanceAPI/BinanceAPI.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICall
+{
+    public class BinanceAPI
+    {
+        HttpClient HttpClient { get; set; }
+
+        IConfiguration Configuration { get; set; }
+
+        private string _apiKey;
+        private string _secret;
+        private string _url;
+
+        public BinanceAPI(HttpClient httpClient, IConfiguration configuration)
+        {
+            HttpClient = httpClient;
+            Configuration = configuration;
+
+            _url = GetConfiguration("Binance:Url");
+            _apiKey = GetConfiguration("Binance:ApiKey");
+            _secret = GetConfiguration("Binance:Secret");
+        }
+
+        public async Task<HttpStatusCode> GetHistoricalTrades()
+        {
+            try
+            {
+                using (var request = new HttpRequestMessage(HttpMethod.Get, _url + "/api/v3/historicalTrades?symbol=ADAUSDT"))
+                {
+                    AddApiKey(request);
+
+                    using (HttpResponseMessage httpResponse = HttpClient.Send(request))
+                    {
+                        string statusCode = httpResponse.StatusCode.ToString();
+                        //Log.Debug("# HTTP RESPONSE : " + statusCode);
+                        //if (statusCode != "OK")
+                        //    Log.Error(httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+                        var strInfo = await httpResponse.Content.ReadAsStringAsync();
+
+                        return httpResponse.StatusCode;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return HttpStatusCode.InternalServerError;
+            }
+        }
+
+        //public async Task<HttpStatusCode> GetAllTrades()
+        //{
+        //    using (var request = new HttpRequestMessage(HttpMethod.Get, _url))
+        //    {
+        //        AddApiKey(request);
+
+        //        request.Content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
+
+        //        using (HttpResponseMessage httpResponse = await HttpClient.SendAsync(request))
+        //        {
+        //            string statusCode = httpResponse.StatusCode.ToString();
+        //            //Log.Debug("# HTTP RESPONSE : " + statusCode);
+        //            //if (statusCode != "OK")
+        //            //    Log.Error(httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
+        //            return httpResponse.StatusCode;
+        //        }
+        //    }
+
+        //}
+
+        private void AddApiKey(HttpRequestMessage message)
+        {
+            message.Headers.Add("X-MBX-APIKEY", _apiKey);
+        }
+
+        private string GetConfiguration(string key)
+        {
+            string val = Configuration[key];
+
+            if (String.IsNullOrEmpty(val))
+                throw new Exception($"Pas de {key} trouvé dans le fichier de configuration");
+
+            return val;
+        }
+
+    }
+}
---
+++ CoinManager/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using APICall;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CoinBase
+{
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IServiceProvider ServiceProvider { get; private set; }
+        public IConfiguration Configuration { get; private set; }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                    .AddJsonFile("Appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddHttpClient<BinanceAPI>();
+
+            services.AddSingleton<MainWindow>();
+
+            services.AddSingleton<IConfiguration>(Configuration);
+        }
+    }
+}
